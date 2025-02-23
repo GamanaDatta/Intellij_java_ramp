6cd679bcb2038c04784dbd46886d473710c5f2db
GamanaDatta <gamanadatta4@gmail.com>
2025-02-23 16:56:14 +0530

    Class Concepts understanding

---
+++ src/Class/Car.java
@@ -0,0 +1,31 @@
+package Class;
+
+public class Car
+{
+    // Fields (State)
+    String brand;
+    int speed;
+
+    // Constructor
+    Car(String brand, int speed) {
+        this.brand = brand;
+        this.speed = speed;
+    }
+
+    // Method (Behavior)
+    void display() {
+        System.out.println("Car Brand: " + brand + ", Speed: " + speed + " km/h");
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object of Car class
+        Car myCar = new Car("Toyota", 120);
+
+        // Calling method on object
+        myCar.display();
+    }
+
+}
---
+++ src/Class/Objects.java
@@ -0,0 +1,13 @@
+package Class;
+
+public class Objects {
+
+    int x = 5;
+
+    public static void main(String[] args) {
+        Objects myObj1 = new Objects();  // Object 1
+        Objects myObj2 = new Objects();  // Object 2
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+    }
+}
